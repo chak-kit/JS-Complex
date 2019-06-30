58f7c21fc5ffa72378a19b16a3ea5e02c2cbf3b0
chak-kit <kitchakk93@gmail.com>
2019-07-01 00:30:37 +0300

    okten

--- Node js/next_node/search.js
+++
@@ -1,6 +0,0 @@
-module.exports.findUsersByName = function (users, name) {
-   let result = users.filter(function (user) {
-        return user.name.toLowerCase().indexOf(name.toLowerCase()) > -1;
-    });
-    return result;
-};
